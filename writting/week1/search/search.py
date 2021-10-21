d2f4ce4b135031350a5d0e4b618b5ea2b269ee05
John-Atha <giannisj3@gmail.com>
2021-10-21 18:52:17 +0300

    Add hill climbing implementation, update dirs structure

--- writting/week1/search/search.py
+++ writting/week1/search/search.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 class Node():
     def __init__(self, name, parent=None, estimation=None):
@@ -31,8 +32,13 @@ class Graph():
             node = Node(name=u)
             self.nodes[u] = node
 
-    def get_neighbours(self, u):
-        return self.edges.get(u)
+    def get_children(self, u):
+        children = set()
+        if self.edges.get(u):
+            for edge in self.edges.get(u):
+                node = edge.get('child')
+                children.add(node)
+        return children
     
     def read_from_file(self, filename):
         with open(filename) as f:
@@ -40,7 +46,7 @@ class Graph():
             if not contents.count('Edges:') or not contents.count('Estimations:'):
                 raise Exception('Invalid input file.')
             contents = contents.splitlines()
-            print(contents)
+            #print(contents)
             estimations_started = False
             for line in contents:
                 if 'Edges:' in line:
@@ -82,15 +88,51 @@ class Frontier():
         if self.empty():
             raise Exception("Tried to remove node from empty frontier.")
 
+# by default, the init state has the label 's', and the goal state has the label 'g'
+def hill_climbing_main(graph):
+
+    visited = set()
+    
+    def hill_climbing_iter(best_node):
+        print('--------------------------------------------------')
+        if (best_node.name=='g'):
+            print("I found the goal.")
+            return
+
+        prev = best_node
+        children = graph.get_children(best_node)
+        best_node = Node(name='Dummy', estimation=math.inf)
+        if (len(children)):
+            for child in children:
+                if float(child.estimation) < float(best_node.estimation):
+                    best_node = child
+        
+        visited.add(prev.name)
+        print("Current state:", prev, end='  ---  ')
+        print("Closed set:", visited, end='  ---  ')
+        if len(children):
+            print("Children:", [str(child) for child in children], end='  ---  ')
+            print(f"I am going to {best_node}")
+            hill_climbing_iter(best_node)
+        else:
+            print("No children, no next state", str(prev))
+            print('--------------------------------------------------')
+            print("Could not reach goal, local minimum at:", str(prev))
+            return
+    
+    print("I am hill climbing, in fact I do not use a frontier, so you will not find a frontier on my table:")
+    hill_climbing_iter(graph.nodes.get('s'))
+    
+
 def solve():
     graph = Graph()
     if len(sys.argv)!=2:
         sys.exit('Usage: python3 search.py <filename>') 
     graph.read_from_file(sys.argv[1])
-    graph.print()
+    #graph.print()
     way = int(input('Which algorithm would you like to use (1, 2 or 3)?\n  1.Hill Climbing\n  2.Best First\n  3.A*\n'))
     if way==1:
-        pass
+        hill_climbing_main(graph)
     elif way==2:
         pass
     elif way==3:
